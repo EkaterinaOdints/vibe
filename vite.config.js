03a76b104030bf37d6d0e51040f5180308df981b
EkaterinaOdints <157913603+EkaterinaOdints@users.noreply.github.com>
2026-03-09 14:21:10 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import { resolve } from "path";
 
 export default defineConfig({
   root: "./src",
@@ -8,6 +9,23 @@ export default defineConfig({
   build: {
     outDir: "../docs",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        catalog: resolve(__dirname, "src/catalog.html"),
+        bottleDetail: resolve(__dirname, "src/bottle-detail.html"),
+        productsDetail: resolve(__dirname, "src/products-detail.html"),
+        about: resolve(__dirname, "src/about.html"),
+        contacts: resolve(__dirname, "src/contacts.html"),
+        delivery: resolve(__dirname, "src/delivery.html"),
+        sales: resolve(__dirname, "src/sales.html"),
+        blog: resolve(__dirname, "src/blog.html"),
+        blogDetail: resolve(__dirname, "src/blog-detail.html"),
+        wholesale: resolve(__dirname, "src/wholesale.html"),
+        reviews: resolve(__dirname, "src/reviews.html"),
+        error: resolve(__dirname, "src/404.html"),
+      },
+    },
   },
   server: {
     open: true,
